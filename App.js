c56c563eb0c863a2f35ddb4ba9309deaf5b91c64
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2023-05-18 22:42:32 +0300

    update: register page

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // import { StatusBar } from 'expo-status-bar';
 // import {StyleSheet, Text, View, Platform, TextInput, ImageBackground} from 'react-native';
-import RegistrationScreen from "./Screens/RegistrationScreen";
+import RegistrationScreen from './Screens/RegistrationScreen';
 
 export default function App() {
   return (
--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -1,13 +1,62 @@
-import React from 'react';
-import { View, Text, StyleSheet, ImageBackground, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  TouchableHighlight,
+} from 'react-native';
+import Add from '../assets/icons/share/add.svg';
 
 const RegistrationScreen = () => {
+  const [isShow, setIsShow] = useState(false);
+
+  const onShowChange = () => {
+    setIsShow((prevState) => !prevState);
+  };
+
   return (
     <View style={styles.container}>
-      <ImageBackground style={styles.bgImg} source={require('../assets/images/main_bg.jpg')}>
-        <View style={styles.box}>
-          <Text>Sign In</Text>
-          <TextInput style={styles.input} />
+      <ImageBackground
+        style={styles.bgImg}
+        source={require('../assets/images/main_bg.jpg')}
+      >
+        <View style={styles.form}>
+          <View style={styles.avatarBox}>
+            <TouchableHighlight style={styles.btnAdd}>
+              <View>
+                <Add width={25} height={25} />
+              </View>
+            </TouchableHighlight>
+          </View>
+          <Text style={styles.title}>SignUp</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Login"
+            placeholderTextColor="#BDBDBD"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            placeholderTextColor="#BDBDBD"
+          />
+          <View style={styles.passwordBox}>
+            <TextInput
+              style={styles.passwordInput}
+              placeholder="Password"
+              secureTextEntry={!isShow}
+              placeholderTextColor="#BDBDBD"
+            />
+            <TouchableOpacity style={styles.isShowBtn} onPress={onShowChange}>
+              <Text style={styles.isShowBtnText}>Show</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.btnText}>SignUp</Text>
+          </TouchableOpacity>
+          <Text style={styles.helpText}>Already have an account? Sign In</Text>
         </View>
       </ImageBackground>
     </View>
@@ -24,20 +73,86 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     resizeMode: 'cover',
   },
-  box: {
-    minHeight: 100,
+  form: {
+    minHeight: 549,
+    paddingTop: 92,
+    paddingLeft: 16,
+    paddingRight: 16,
     borderTopStartRadius: 25,
     borderTopEndRadius: 25,
     backgroundColor: '#fff',
     position: 'relative',
   },
+  avatarBox: {
+    position: 'absolute',
+    alignSelf: 'center',
+    width: 120,
+    height: 120,
+    top: -60,
+    borderRadius: 16,
+    backgroundColor: '#F6F6F6',
+  },
+  btnAdd: {
+    position: 'absolute',
+    right: -12.5,
+    bottom: 14,
+  },
+  title: {
+    fontWeight: '500',
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: 'center',
+    letterSpacing: 0.01,
+    color: '#212121',
+    marginBottom: 33,
+  },
   input: {
-    flex: 1,
-    height: 50,
+    height: 20,
+    padding: 18,
     backgroundColor: '#F6F6F6',
     borderRadius: 8,
     borderWidth: 1,
     borderColor: '#E8E8E8',
+    marginBottom: 16,
+  },
+  passwordBox: {
+    position: 'relative',
+    marginBottom: 43,
+  },
+  passwordInput: {
+    height: 20,
+    padding: 18,
+    backgroundColor: '#F6F6F6',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#E8E8E8',
+  },
+  isShowBtn: {
+    position: 'absolute',
+    right: 0,
+    justifyContent: 'center',
+    padding: 8,
+    top: 0,
+    bottom: 0,
+  },
+  isShowBtnText: {},
+  button: {
+    marginHorizontal: 'auto',
+    backgroundColor: '#FF6C00',
+    height: 51,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+  },
+  btnText: {
+    color: '#ffffff',
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  helpText: {
+    marginTop: 16,
+    textAlign: 'center',
+    color: '#1B4371;',
   },
 });
 
